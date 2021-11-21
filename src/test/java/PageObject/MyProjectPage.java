699d9bab7b80170c6a3a2d31d488b112326004bd
juanquiceno2000 <juan.quiceno@yuxiglobal.com>
2021-11-21 17:57:12 -0500

    Refactor

--- src/test/java/PageObject/MyProjectPage.java
+++ src/test/java/PageObject/MyProjectPage.java
@@ -6,6 +6,6 @@ import org.openqa.selenium.By;
 public class MyProjectPage extends PageObject {
 
     static{
-        System.setProperty("webdriver.edge.driver","C:/Users/juanu/Documentos/GitHub/SerenityBDD/src/test/resources/webdrivers/msedgedriver.exe");
+        System.setProperty("webdriver.edge.driver","src/test/resources/webdrivers/msedgedriver.exe");
     }
 }
